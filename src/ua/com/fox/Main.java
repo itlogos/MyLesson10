519b2c721f52e526fc4d287e36dfd3dc8aedbe47
olegnik <olegnik11@ukr.net>
2023-08-17 18:56:37 +0300

    преобразуем строку в токены

--- src/ua/com/fox/Main.java
+++ src/ua/com/fox/Main.java
@@ -5,32 +5,34 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input sentence : ");
-        String sentence = scanner.nextLine();
-        int worldCount = countWorld(sentence);
-        System.out.println("Count world : " + worldCount);
-        worldCount = countWorld2(sentence);
-        System.out.println("Count world : " + worldCount);
-        scanner.close();
+        String sentence = "Привет мир привет всем";  // исходное предложение
+        System.out.println("Наиболее часто встречающееся слово: " + findMostCommon(sentence));  // вывод результата
     }
 
-    public static int countWorld(String sentence) {
-        //First
-        if (sentence == null || sentence.trim().isEmpty()) {
-            return 0;
+    public static String findMostCommon(String sentence) {
+        StringTokenizer tokenizer = new StringTokenizer(sentence.toLowerCase());  // преобразуем строку в токены, предварительно переведя ее в нижний регистр
+        String[] words = new String[tokenizer.countTokens()];  // массив для слов
+        int i = 0;
+        while (tokenizer.hasMoreTokens()) {  // цикл по всем токенам
+            words[i++] = tokenizer.nextToken();  // сохраняем каждое слово в массив
         }
-        String[] words = sentence.split("\\s+");
-        return words.length;
-    }
 
-    public static int countWorld2(String sentence){
-        //Second
-        if (sentence == null || sentence.trim().isEmpty()) {
-            return 0;
+        int maxCount = 0;  // максимальное количество повторений
+        String mostCommonWord = "";  // самое часто встречающееся слово
+        for (i = 0; i < words.length; i++) {  // цикл по всем словам
+            int count = 0;  // текущее количество повторений слова
+            for (int j = 0; j < words.length; j++) {  // вложенный цикл для подсчета повторений
+                if (words[i].equals(words[j])) {  // если слова совпадают
+                    count++;  // увеличиваем счетчик повторений
+                }
+            }
+            if (count > maxCount) {  // если текущее слово встречается чаще, чем предыдущий рекордсмен
+                maxCount = count;  // обновляем максимальное количество повторений
+                mostCommonWord = words[i];  // сохраняем текущее слово как самое часто встречающееся
+            }
         }
-        StringTokenizer tokenizer = new StringTokenizer(sentence);
-        return tokenizer.countTokens();
+
+        return mostCommonWord;  // возвращаем результат
     }
 
 }
